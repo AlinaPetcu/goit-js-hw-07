cf453caf0a3a1634f60eba778a8507d5240cb3a1
AlinaPetcu <150749536+AlinaPetcu@users.noreply.github.com>
2024-04-15 02:22:37 +0300

    final almost

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-
-
 const list = document.querySelector(".gallery");
 
 function imagePaletteItems() {
@@ -11,6 +9,7 @@ function imagePaletteItems() {
         const image = document.createElement("img");
         image.classList.add("gallery__image");
         image.setAttribute("src", photo.preview);
+        image.setAttribute("data-source", photo.original);
         image.setAttribute("alt", photo.description);
 
         //creare tag li + atribute/clasa
@@ -31,12 +30,22 @@ function imagePaletteItems() {
         link.addEventListener("click", (event) => {
         event.preventDefault();
         });
-
-        //fereastra modala cu imaginea completa
-        image.onclick = () => {
-            basicLightbox.create(`<img width="800" height="600" src="${photo.original}">`
-            ).show()
-        }       
+        
+        let instance;
+        //deschidere fereastra modala cu imaginea completa
+        image.addEventListener("click", () => {
+        instance = basicLightbox.create(`<img width="800" height="600" src="${photo.original}">`)
+        instance.show();
+        }); 
+        
+         //inchidere fereastra modala cu imaginea completa  
+        document.addEventListener("keyup", (event) => {
+            if (event.code === "Escape") {
+                instance.close();
+            }
+        })
+       
+        
     }
  
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const list = document.querySelector(".gallery");
 
+function imagePaletteItems() {
+    for (const photo of galleryItems) {
+        //creare tag img + atribute/clasa
+        const image = document.createElement("img");
+        image.classList.add("gallery__image");
+        image.setAttribute("src", photo.preview);
+        image.setAttribute("alt", photo.description);
+
+        //creare tag li + atribute/clasa
+        const elem = document.createElement("li");
+        elem.classList.add("gallery__item");
+
+        //creare tag a + atribute/clasa
+        const link = document.createElement("a");
+        link.setAttribute("href", photo.original);
+        link.classList.add("gallery__link");
+  
+        //aranjare elemente
+        elem.appendChild(link);
+        link.appendChild(image);
+        list.appendChild(elem);
+
+        //Dezactivare comportament redirectionare/download
+        link.addEventListener("click", (event) => {
+        event.preventDefault();
+        });
+        
+        const lightbox = new SimpleLightbox('.gallery a', {overlayOpacity:'0.4', navText:['&#10094;','&#10095;'], docClose:false,history:false, alertError:false});
+       
+        //const lightbox = new SimpleLightbox('.gallery a', { sourceAttr: 'href', overlay:'true', overlayOpacity:'0.7',nav:'true', navText:['<','>'], captionSelector:'img',close: 'true', closeText:'×'});
+       
+        
+    }
+ 
+}
+
+imagePaletteItems();
 console.log(galleryItems);
